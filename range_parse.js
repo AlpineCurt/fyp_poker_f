4ef052737b7024e482bcbf6576cf73612fd11f2f
Curt Davidson <curt@pipernetworks.com>
2025-05-15 19:16:45 -0700

    hehehawhaw

--- range_parse.js
+++ range_parse.js
@@ -1,8 +1,24 @@
-function range_parse(range, text = true) {
+const suits_str = ["s", "c", "h", "d"];
+const suits_int = [1, 2, 3, 4];
+
+function order_suits(s1, s2) {
+  const idx1 = suits_str.indexOf(s1);
+  const idx2 = suits_str.indexOf(s2);
+  if (idx1 === -1 || idx2 === -1) {
+    throw "derp.";
+  }
+  if (idx1 < idx2) {
+    return [s1, s2];
+  } else {
+    return [s2, s1];
+  }
+}
+
+function range_parse(range) {
   range = range.replace(/ /g, "");
   range = range.split(",");
-  let all_combos = [];
-  let all_bad_combos = [];
+  let all_combos = new Set();
+  let all_bad_combos = new Set();
   for (const hands of range) {
     let combos = [];
     let bad_combos = [];
@@ -36,17 +52,15 @@ function range_parse(range, text = true) {
         break;
       case 10:
         combos = parse_single_combo(hands);
+        break;
       case -1:
-        all_bad_combos.push(hands);
+        all_bad_combos.add(hands);
         break;
     }
-    all_combos = [...all_combos, ...combos];
-    all_bad_combos = [...all_bad_combos, ...bad_combos];
+    combos.forEach((c) => all_combos.add(c));
+    bad_combos.forEach((c) => all_bad_combos.add(c));
   }
-  console.log("stop");
-  // need to handle duplicates, or check for them or something.
-  // need to ensure there are no dublicates.  there we go. that's what I wanted to say.
-  // ex: QQ+ and then 33+ would be weird, but is valid.
+  return [all_combos, all_bad_combos];
 }
 
 /**
@@ -104,6 +118,7 @@ function parse_pp_plus(hands) {
   return combos;
 }
 function parse_pp_single(hands) {
+  // need to standardize the order of suits for combos
   let combos = [];
   const rank = face_to_value[hands[0]];
   for (let j = 0; j < 4; j++) {
@@ -111,8 +126,9 @@ function parse_pp_single(hands) {
       if (j === k) {
         continue;
       }
+      const so = order_suits(suits_str[j], suits_str[k]);
       combos.push(
-        `${value_to_face[rank]}${suits_str[j]}${value_to_face[rank]}${suits_str[k]}`
+        `${value_to_face[rank]}${so[0]}${value_to_face[rank]}${so[1]}`
       );
     }
   }
@@ -216,16 +232,51 @@ function parse_offsuit_range(hands) {
     bad_combos.push(hands);
     return [combos, bad_combos];
   }
+  const rank1 = hands[0];
+  const high_rank = Math.max(face_to_value[hands[1]], face_to_value[hands[5]]);
+  const low_rank = Math.min(face_to_value[hands[1]], face_to_value[hands[5]]);
+  for (let i = low_rank; i <= high_rank; i++) {
+    for (const suit1 of suits_str) {
+      for (const suit2 of suits_str) {
+        if (suit1 === suit2) {
+          continue;
+        }
+        combos.push(`${rank1}${suit1}${value_to_face[i]}${suit2}`);
+      }
+    }
+  }
   return [combos, bad_combos];
 }
 
 function parse_offsuit_single(hands) {
   let combos = [];
+  const high_rank = Math.max(face_to_value[hands[0]], face_to_value[hands[1]]);
+  const low_rank = Math.min(face_to_value[hands[0]], face_to_value[hands[1]]);
+  for (const suit1 of suits_str) {
+    for (const suit2 of suits_str) {
+      if (suit1 === suit2) {
+        continue;
+      }
+      combos.push(
+        `${value_to_face[high_rank]}${suit1}${value_to_face[low_rank]}${suit2}`
+      );
+    }
+  }
   return combos;
 }
 
 function parse_single_combo(hand) {
   let combos = [];
+  if (face_to_value[hand[0]] > face_to_value[hand[2]]) {
+    combos.push(hand);
+  } else if (face_to_value[hand[0]] < face_to_value[hand[2]]) {
+    const combo = `${hand[2]}${hand[3]}${hand[0]}${hand[1]}`;
+    combos.push(combo);
+  } else {
+    const co = order_suits(hand[1], hand[3]);
+    const combo = `${hand[0]}${co[0]}${hand[0]}${co[1]}`;
+    combos.push(combo);
+  }
   return combos;
 }
 
@@ -275,10 +326,122 @@ const value_to_face = {
   14: "A",
 };
 
-const suits_str = ["s", "c", "h", "d"];
-const suits_int = [1, 2, 3, 4];
+function build_combos_obj() {
+  const ranks = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+  const res = {};
+  for (let i = 0; i < ranks.length; i++) {
+    for (let j = i; j < ranks.length; j++) {
+      if (i === j) {
+        res[`${value_to_face[ranks[i]]}${value_to_face[ranks[j]]}`] = [];
+      } else {
+        res[`${value_to_face[ranks[i]]}${value_to_face[ranks[j]]}s`] = [];
+        res[`${value_to_face[ranks[i]]}${value_to_face[ranks[j]]}o`] = [];
+      }
+    }
+  }
+  return res;
+}
+
+const comb_obj = build_combos_obj();
 
 const range =
-  "t8o-tao, AQo+, AJs-A3s, 55-33, KJs, A2s+, KK+, 88, ATo+, K8o-KTo, 5h4h";
-const res = range_parse(range);
+  "2h7d, T8o, K8o-KTo, AQo+, ATs-AQs, 55-33, KJs, Q8s+, KK+, 22-44, 88, ATo+, 5h4h, 7h7c, 7c7h";
+const [all_combos, all_bad_combos] = range_parse(range);
+
+function fill_combos_obj(comb_obj, combos) {
+  for (const c of combos) {
+    if (c[0] === c[2]) {
+      comb_obj[`${c[0]}${c[0]}`].push(c);
+    } else if (c[1] === c[3]) {
+      comb_obj[`${c[0]}${c[2]}s`].push(c);
+    } else {
+      comb_obj[`${c[0]}${c[2]}o`].push(c);
+    }
+  }
+}
+
+fill_combos_obj(comb_obj, all_combos);
+
+function combo_obj_to_range(co) {
+  let rs = "";
+  let ind_combos = [];
+  const ranks = ["A", "K", "Q", "J", "T", 9, 8, 7, 6, 5, 4, 3, 2];
+
+  // pocket pairs
+  let i, j, k;
+  for (i = 0; i < ranks.length; i = j) {
+    let upper_rank, lower_rank;
+    const rank1 = ranks[i];
+    if (co[`${rank1}${rank1}`].length === 6) {
+      upper_rank = rank1;
+    } else {
+      if (co[`${rank1}${rank1}`].length > 0) {
+        ind_combos = [...ind_combos, ...co[`${rank1}${rank1}`]];
+      }
+      j++;
+      continue;
+    }
+
+    for (j = i + 1; j < ranks.length; j++) {
+      const rank2 = ranks[j];
+      if (co[`${rank2}${rank2}`].length !== 6) {
+        break;
+      }
+      lower_rank = rank2;
+    }
+    if (!lower_rank) {
+      rs += `${upper_rank}${upper_rank},`;
+    } else if (upper_rank && lower_rank) {
+      if (upper_rank === "A") {
+        rs += `${lower_rank}${lower_rank}+,`;
+      } else {
+        rs += `${lower_rank}${lower_rank}-${upper_rank}${upper_rank},`;
+      }
+    }
+  }
+
+  for (const os of ["s", "o"]) {
+    for (i = 0; i < ranks.length; i++) {
+      let upper_rank, lower_rank;
+      const rank1 = ranks[i];
+      for (j = i + 1; j < ranks.length; j++) {
+        if (co[`${rank1}${ranks[j]}${os}`].length === (os === "s" ? 4 : 12)) {
+          upper_rank = ranks[j];
+          for (k = j + 1; k < ranks.length; k++) {
+            if (
+              co[`${rank1}${ranks[k]}${os}`].length === (os === "s" ? 4 : 12)
+            ) {
+              lower_rank = ranks[k];
+              continue;
+            }
+            if (lower_rank) {
+              if (j === i + 1) {
+                rs += `${rank1}${lower_rank}${os}+,`;
+              } else {
+                rs += `${rank1}${upper_rank}${os}-${rank1}${lower_rank}${os},`;
+              }
+              j = k - 1;
+            } else {
+              rs += `${rank1}${upper_rank}${os},`;
+            }
+            break;
+          }
+        } else {
+          if (co[`${rank1}${ranks[j]}${os}`].length > 0) {
+            ind_combos = [...ind_combos, ...co[`${rank1}${ranks[j]}${os}`]];
+          }
+          continue;
+        }
+      }
+    }
+  }
+  // left off here. need to do offsuit
+  // can use same code as above and/or add an additional loop for o vs s
+  ind_combos.forEach((c) => (rs += `${c},`));
+  rs = rs.slice(0, -1);
+  return rs;
+}
+
+const rs = combo_obj_to_range(comb_obj);
+
 console.log("stop");
